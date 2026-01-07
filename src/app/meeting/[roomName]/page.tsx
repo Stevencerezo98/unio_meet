9c986eee3a1a1106a240ad1c624a54bc3f4b734f
steven <stevencerezo42@gmail.com>
2026-01-07 06:15:48 +0000

    Try fixing this error: `Console Error: A param property was accessed dir

--- src/app/meeting/[roomName]/page.tsx
+++ src/app/meeting/[roomName]/page.tsx
@@ -6,11 +6,10 @@ import { useSearchParams } from 'next/navigation';
 export default function MeetingPage({ params }: { params: { roomName: string } }) {
   const searchParams = useSearchParams();
   const userName = searchParams.get('userName') || 'Guest';
-  const roomName = decodeURIComponent(params.roomName);
 
   return (
     <main>
-      <MeetingRoom roomName={roomName} userName={userName} />
+      <MeetingRoom roomName={decodeURIComponent(params.roomName)} userName={userName} />
     </main>
   );
 }
